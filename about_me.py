219b5f0183c07a3a824225faa2bb3728a89d4eea
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2026-03-21 21:58:53 +0530

    Day 01 - Python basics

---
+++ about_me.py
@@ -0,0 +1,6 @@
+name = "A.Srinidhi"
+degree = "Masters in Data Science"
+favourite_topic = "Python"
+target_country = "UK"
+paragraph = f"My name is {name}. I completed my masters in {degree}. My favourite topic is {favourite_topic}. I want to purse my PhD in {target_country}."
+print(paragraph)
---
+++ day01_python_basics.py
@@ -0,0 +1,28 @@
+"""
+variables
+Name = "A.Srinidhi"
+Age = 24
+Why = "I want to be the first peron and an inspiration for my generation and my family by pursuing the highest level of education "
+print("Name:", Name)
+print("Age:", Age)
+print("Why PhD:", Why)
+
+data types
+num1 = float(input("Enter your number: "))
+num2 = float(input("Enter your number: "))
+print("Sum:",  num1 + num2)
+print("Diff:",  num1 - num2)
+print("Product:",  num1 * num2)
+print("Quotient:",  num1 / num2)
+
+IP/OP
+celsius = float(input("Enter temp in celsius: "))
+fahrenheit = (9/5) * celsius + 32
+print("Temperature in Fahrenheit:", fahrenheit)
+
+String length & upper case
+x = "Srinidhi"
+print(len(x))
+print(x.upper())
+
+"""
