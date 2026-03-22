3ae1366083c5e35bb91c055f559141bdebff72e4
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2026-03-23 00:20:11 +0530

    day02_python conditions and operators

---
+++ day02_python_operators_conditions.py
@@ -0,0 +1,38 @@
+"""
+#Program to check if a number is positive, negative, or zero.
+num = float(input("Enter a num: "))
+if num > 0:
+    print("the number is positive")
+elif num < 0:
+    print("the number is negative")
+else:
+    print("it is a zero")
+
+#Program to check if a year is leap year.
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 4 == 0):
+    print("It is leap year")
+else:
+    print("It is not a leap year")
+
+#Program to compute grade from marks (A/B/C/F).
+Marks = int(input("Enter your marks: "))
+if Marks >= 95:
+    print("A grade")
+elif Marks >= 75:
+    print("B grade")
+elif Marks >= 50:
+    print("C grade")
+else:
+    print("F grade")
+
+#Simple login simulation: ask for username/password; if matches hardcoded values, print “Login success”.
+correct_username = "Sri25"
+correct_password = "Sri@250518"
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if correct_username == username:
+    print("Logic success")
+else:
+    print("Incorrect password/username")
+"""
---
+++ study_planner.py
@@ -0,0 +1,7 @@
+study_hours = int(input("How many hrs you can study today: "))
+if study_hours >= 4:
+    print("Intense study")
+elif study_hours >= 2:
+    print("Moderate study")
+else:
+    print("Light study")
