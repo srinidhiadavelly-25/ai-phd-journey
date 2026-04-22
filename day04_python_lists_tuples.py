c7a1c1ed87f646a6fb97a4d985dd66923ac5520b
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2026-04-22 15:27:15 +0530

    Day 04_ python lists & tuples

---
+++ day04_python_lists_tuples.py
@@ -0,0 +1,27 @@
+#Question 1 – Target universities list
+universities = ["UCL", "Bristol", "Birmingham", "Nephier", "Oxford"]
+for i in universities:
+    print("My target universities are: ", i)
+
+#Question 2 – Numbers: max, min, average
+numbers = []
+total = 0
+for i in range(5):
+    n = int(input("Enter number: "))
+    numbers.append(n)
+    total = total+n
+Average = total/5
+print("Max: ", max(numbers))
+print("Min: ", min(numbers))
+print("Average: ", Average)
+
+#Question 3 – Reverse a list without using reverse()
+fruits = ["apple", "kiwi", "cherry", "guava"]
+reversed_list = fruits[::-1]
+print(reversed_list)
+
+#Question 4 – Tuple with basic info
+my_info = ("Srinidhi", 24, "Data Science" )
+print(my_info[0])
+print(my_info[1])
+print(my_info[2])
---
+++ phd_targets.py
@@ -0,0 +1,11 @@
+#phd target ares
+research_areas = ["AI","ML", "DS", "CS"]
+for area in research_areas:
+    print(area)
+n = int(input("How many research areas you want to add: "))
+for i in range(n):
+    new_area =  input("Enter research area: ")
+    research_areas.append(new_area)
+print("Final list of research ares: ")
+for area in research_areas:
+    print(area)
