814a43ad16982127d81ecadcee8d66a567144cba
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2026-04-22 15:29:42 +0530

    Day 04_ python lists & tuples

--- phd_targets.py
+++ phd_targets.py
@@ -6,6 +6,6 @@ n = int(input("How many research areas you want to add: "))
 for i in range(n):
     new_area =  input("Enter research area: ")
     research_areas.append(new_area)
-print("Final list of research ares: ")
+print("Final list of research areas: ")
 for area in research_areas:
     print(area)
